7ff1a46a563f7198cf5a2c1d1d56c3c6f3e895b1
huebner0 <martin130486@gmail.com>
2017-12-05 16:24:10 +0100

    like a little setup

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,11 @@ module Main where
 
 import Lib
 
+import System.IO (hSetBuffering, BufferMode(NoBuffering), stdout)
 main :: IO ()
-main = someFunc
+main = do
+  putStr "Geben Sie bitte einen Dateinamen an: "
+  fileName <- getLine
+  openFile fileName
+  putStrLn (" Die Datei " ++ fileName ++ " soll geoeffnet werden.")
+--  hSetBuffering stdout NoBuffering
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,11 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+    import System.IO
+    
+    openFile :: String -> IO ()
+    openFile fileName = withFile "Lorem_Ipsum.txt" ReadMode $ \handle -> do
+                  xs <- getlines handle
+                  sequence_ $ map putStrLn xs
+
+    getlines :: Handle -> IO [String]
+    getlines h = hGetContents h >>= return . lines
