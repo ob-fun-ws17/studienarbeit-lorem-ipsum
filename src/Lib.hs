8af615282afd06c076042a856736eb6a7b66ef95
Florian Seeholzer <florian.seeholzer@gmx.de>
2017-12-12 16:03:27 +0100

    Initialized map to manage the occurences of the different letter combinations

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,21 @@
 module Lib where
 
     import System.IO
+    import qualified Data.Map as M
+
+    type Key    = Char
+    type Occurences  = M.Map Key Integer
+    type Letter = Char
+    type Alphabet = M.Map Letter Occurences
+
+    alphabet :: Alphabet
+    alphabet = M.fromList[('A', M.fromList[('A', 0)])]
+
+    initOccurences :: Occurences
+    initOccurences = foldr(uncurry M.insert) M.empty $ zip ['A', 'B'..'Z'] $ repeat 0
+
+    initAlphabet :: Alphabet
+    initAlphabet = foldr(uncurry M.insert) M.empty $ zip ['A', 'B'..'Z'] $ repeat initOccurences
 
     openFile :: String -> IO ()
     openFile fileName = withFile "LoremIpsum.txt" ReadMode $ \handle -> do
