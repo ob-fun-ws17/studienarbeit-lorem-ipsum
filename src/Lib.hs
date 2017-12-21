358a3d9d21b660f073d745f7ed9e2e680ba7a58c
Florian Seeholzer <florian.seeholzer@gmx.de>
2017-12-21 18:38:58 +0100

    TODO update map.

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,13 +3,12 @@ module Lib where
     import System.IO
     import qualified Data.Map as M
 
-    type Key    = Char
-    type Occurences  = M.Map Key Integer
-    type Letter = Char
-    type Alphabet = M.Map Letter Occurences
+    type Key           = Char
+    type Occurences    = M.Map Key Integer
+    type Letter        = Char
+    type Alphabet      = M.Map Letter Occurences
 
-    alphabet :: Alphabet
-    alphabet = M.fromList[('A', M.fromList[('A', 0)])]
+    alphabet = initAlphabet
 
     initOccurences :: Occurences
     initOccurences = foldr(uncurry M.insert) M.empty $ zip ['A'..'Z'] $ repeat 0
@@ -24,3 +23,12 @@ module Lib where
 
     getlines :: Handle -> IO [String]
     getlines h = hGetContents h >>= return . lines
+
+    getLength :: String -> Int
+    getLength s = length s
+
+    --getOccurence :: Key -> Integer
+    --getOccurence x = (M.findWithDefault 0 x initOccurences)
+
+    --incrementAlphabet :: Key -> Alphabet
+    --incrementAlphabet = M.updateWithKey 'A' alphabet
