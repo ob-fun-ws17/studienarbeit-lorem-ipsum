68bd1285fb155ca68f3d9b2f01a808e62d452f4e
huebner0 <martin130486@gmail.com>
2018-01-28 20:08:14 +0100

    Tests erweitert

--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -7,6 +7,40 @@ import Test.QuickCheck
 
 spec :: Spec
 spec = do
+
+  let alpha_A1 = ('A', 0.1)
+  let alpha_B2 = ('B', 0.2)
+  let alpha_C3 = ('C', 0.3)
+  let alpha_D4 = ('D', 0.4)
+  let alpha_B5 = ('B', 0.5)
+  let alpha_C6 = ('C', 0.6)
+  let alpha_D7 = ('D', 0.7)
+  let alpha_A8 = ('A', 0.8)
+  let alpha_D9 = ('D', 0.9)
+
+  let alpha_AA1 = ('A', alpha_A1)
+  let alpha_AC6 = ('A', alpha_C6)
+  let alpha_AD9 = ('A', alpha_D9)
+
+  let alpha_BD4 = ('B', alpha_D4)
+  let alpha_BB5 = ('B', alpha_B5)
+  let alpha_BC6 = ('B', alpha_C6)
+
+  let alpha_CC1 = ('C', alpha_C3)
+  let alpha_CB5 = ('C', alpha_B5)
+  let alpha_CD9 = ('C', alpha_D9)
+
+  let alpha_DD4 = ('D', alpha_D4)
+  let alpha_DB5 = ('D', alpha_B5)
+  let alpha_DC6 = ('D', alpha_C6)
+
+  let alpha_AList = [alpha_AA1, alpha_AC6, alpha_AD9]
+  let alpha_BList = [alpha_BD4, alpha_BB5, alpha_BC6]
+  let alpha_CList = [alpha_CC1, alpha_CB5, alpha_CD9]
+  let alpha_DList = [alpha_DD4, alpha_DB5, alpha_DC6]
+
+  let alpha_List = [alpha_AList, alpha_BList, alpha_CList, alpha_DList]
+
   --describe "getSubTupelOfTuple" $ do
   describe "getSubTupelOfTuple :: (Char, (Char, Float)) -> (Char, Float)" $ do
     it "calculates the getSubTupelOfTuple of ('A', ('B', 3.1415))" $
